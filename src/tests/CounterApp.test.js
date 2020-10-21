3c8d83173a46e451a4c8b21e9d6a1a563da7d248
Rodrigo <rodrigoa459@gmail.com>
2020-10-21 18:26:59 -0500

    Finalización de las pruebas en componentes

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -5,9 +5,15 @@ import CounterApp from "../CounterApp";
 
 describe('Pruebas realizadas a <CounterApp />', () => {
 
-    test('Debe cargar <CounterApp /> con el valor por defecto de 10', () => {
+    let wrapper;
+
+    beforeEach( () => {
+
+        wrapper = shallow( <CounterApp /> );
         
-        const wrapper = shallow(<CounterApp />);
+    });
+
+    test('Debe cargar <CounterApp /> con el valor por defecto de 10', () => {
 
         expect( wrapper ).toMatchSnapshot();
 
@@ -23,5 +29,41 @@ describe('Pruebas realizadas a <CounterApp />', () => {
         expect( valorRenderizado ).toBe( valor.toString() );
 
     });
+
+    test('Debe incrementar el contador que se renderizo', () => {
+
+        const btn1 = wrapper.find('button').at(0).simulate('click');
+
+        const valorRenderizado = wrapper.find('h2').text();
+        
+        expect( valorRenderizado ).toBe('11');
+        
+    });
+
+    test('Debe decrementar el contador que se renderizo', () => {
+
+        
+        const btn1 = wrapper.find('button').at(2).simulate('click');
+
+        const valorRenderizado = wrapper.find('h2').text();
+        
+        expect( valorRenderizado ).toBe('9');
+        
+    });
+
+    test('Debe de asignar el valor de los props en el contador renderizado', () => {
+        
+        const wrapper = shallow(<CounterApp value={ 105 } />);
+
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(1).simulate('click');
+        const valorRenderizado = wrapper.find('h2').text();
+
+        expect( valorRenderizado ).toBe('105');
+
+    })
+    
+    
 
 })
